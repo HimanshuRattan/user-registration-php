6b89e9d776b5026f2a62b36e42d65b67b7a0daeb
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2019-07-08 00:17:45 +0530

    added hashing

--- validation.php
+++ validation.php
@@ -8,10 +8,18 @@ $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 $name="{$_POST['user']}";
 $pass="{$_POST['password']}";
-$ar=$mysqli->query("SELECT * from users WHERE username = '$name' && passcode='$pass'");
+$ar=$mysqli->query("SELECT * from users WHERE username = '$name'");
 if($ar->num_rows) {
-    $_SESSION['username']=$name;
+    // $result = $mysqli->query("SELECT * FROM users WHERE username = '$name'");
+    $row= mysqli_fetch_array( $ar);
+    if(password_verify($pass, $row['passcode'])) {
+     $_SESSION['username']=$name;
     header('location:home.php');
+    }
+    else {
+        $message = "Incorrect Password";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+    }
 }
 else {
    $message = "Incorrect Username or Password";

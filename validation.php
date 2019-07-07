c3c78e3d330f29fd13781368efe9663f7c439cd3
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2019-07-08 00:22:38 +0530

    updated connection details

--- validation.php
+++ validation.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$db_username = 'epiz_24113595';
-$db_password = 'Lam6azQtxB7X';
-$db_name = 'epiz_24113595_registration';
-$db_host = 'sql108.epizy.com';
+$db_username = '';
+$db_password = '';
+$db_name = '';
+$db_host = '';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
 $name="{$_POST['user']}";
